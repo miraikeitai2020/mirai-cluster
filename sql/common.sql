1fc3522f830e9923eb74ad5264d8c2757396e5ed
SaKu2110 <b1018106@fun.ac.jp>
2020-10-25 20:12:26 +0900

    [Add] common sql

---
+++ sql/common.sql
@@ -0,0 +1,18 @@
+---- create database ----
+DROP DATABASE IF EXISTS user;
+CREATE DATABASE user;
+
+---- create user ----
+CREATE USER IF NOT EXISTS 'miraiketai2020'@'%' IDENTIFIED BY 'miraiketai2020';
+GRANT ALL ON user.* TO miraiketai2020;
+
+---- create tables ----
+USE michishirube;
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE IF NOT EXISTS `users`
+(
+ `id`               VARCHAR(256),
+ `pass`             VARCHAR(256) NOT NULL,
+ PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
